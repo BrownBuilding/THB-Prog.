40bce22bf46ae5ffdc198faa3df4a17f09a00205
Bisher <72573608+bisher0707@users.noreply.github.com>
2020-11-08 17:30:06 +0100

    Update Testat Zinsberechnung.java

--- Testat Zinsberechnung.java
+++ Testat Zinsberechnung.java
@@ -1,29 +1,22 @@
+//Danke an Hanna
+
 import java.util.Scanner;
 public class test {
     public static void main (String args []) {
-        // Scanner bereitstellen
         Scanner sc = new Scanner(System.in);
-        //Variable deklarieren
-        //Float falls das Kapital ist eine Zahl mit Komma
-        float Kn,K0;
-        int n,i;
-        //Fragen nach den Eingaben
+        float Kn,K0,i;
+        int n;
         System.out.println("Bitte geben Sie das Anfangskapital ein");
         K0 = sc.nextFloat();
         System.out.println("Bitte geben Sie die Laufzeit in Jahren ein");
         n = sc.nextInt();
         System.out.println("Bitte geben Sie den Zinssatz ein");
-        i = sc.nextInt();
-      
+        i = sc.nextFloat();
         if(i<10){
-                    //Endkapital rechnen und zeigen
-                     Kn = K0+K0*n*i/100;
+                      Kn = K0+K0*n*i/100;
                      System.out.println("Ihr Endkaptial ist "+Kn);
-                     
         }else{
             System.out.println("Eingabefehler");
-            
-            
         }
     }
 }
