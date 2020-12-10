9d0e08c58bee4ade83692979dc3c4c16e22e3ff8
Bisher <72573608+bisher0707@users.noreply.github.com>
2020-12-11 00:07:52 +0100

    Update Body Maß.java

--- Body Maß.java
+++ Body Maß.java
@@ -14,13 +14,17 @@ public class test {
         double groesse = sc.nextDouble();
         
         if(gewicht>0 && groesse>0){
+            // Gewicht und Größe müssen größer als 0 kg sein um bmi zu berechnen
             double bmi = gewicht/(groesse*groesse);
+            
         if(bmi<=20.0 && groesse>1.75){
+            // Kriterien für annhemen oder ablehnen
             System.out.println("Kandidatin angenommen");
         }else{
             System.out.println("Kandidatin abgelehnt");
         }
         }else{
+            // bezieht sich auf die erste IF Anweisung - mit >0 Bedingung 
             System.out.println("Eingabefehler");
         }
     }
