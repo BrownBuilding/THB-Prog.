01435de02d84b7f4aad6722175a1ef5eae0fba8e
Bisher <72573608+bisher0707@users.noreply.github.com>
2020-12-10 22:53:03 +0100

    Update Kreisumfang berechnen.java

--- Kreisumfang berechnen.java
+++ Kreisumfang berechnen.java
@@ -1,21 +1,30 @@
 import java.lang.Math;
+// Es wurde gefordert die Klasse Math zu importieren
 import java.util.Scanner;
+// Scanner brauchen wir um etwas in der Konsole einzugeben
 
 
 public class test{
     public static void main (String [] args){
+        
     final double pi = 3.141592653589793;
-    
+    // final weil double in unserer Aufgabe konstat sein muss
+        
     Scanner sc = new Scanner(System.in);
-    
+        // Scanner mit "sc" aufrufen    
+     
     System.out.println("Gib mal den Radius ein");
+        
     double radius = sc.nextDouble();
+        // radius hat den Wert was der Nutzer in der Konsole eingibt
     
     if(radius>0){
-        System.out.println("Umfang: "+pi*radius*2);
+        System.out.println("Umfang: "+pi*radius*2); 
+        // wenn Radius größer als 0 ist, dann berechnen wir weiter mit Hilfe des Formels auf Moode und zeigen wir das Ergebnis mit System.out.println
         
     }else{
         System.out.println("Eingabefehler");
+        // wenn Radius kleiner als 0 ist, wird nur ein Fehler gezeigt und nichts berechnet
     }
     
 }
