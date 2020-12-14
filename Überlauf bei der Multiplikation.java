31c9cec7999a88f510753ee10aeacf5456ca84c9
Bisher <72573608+bisher0707@users.noreply.github.com>
2020-12-14 19:13:52 +0100

    Create Überlauf bei der Multiplikation.java

---
+++ Überlauf bei der Multiplikation.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+// Scanner brauchen wir um etwas in der Konsole einzugeben
+
+public class test {
+    
+    public static void main (String [] args){
+    
+    final int x = Integer.MAX_VALUE;
+    // final = konstante Variable
+    
+    Scanner sc =  new Scanner(System.in);
+    // Scanner mit "sc" aufrufen
+    System.out.println("Gib mal zwei Zahlen");
+    
+    int y = sc.nextInt();
+    int z = sc.nextInt();
+    
+    if(y>=10 && y<=2000000000 && z>=10 && z<=2000000000){
+        if((y*z)<x || (y*z)<=x){
+            int m = y*z;
+            System.out.println(m);
+        }else{
+            System.out.println("Zahlen zu gross");
+            // wenn die Zahlen zu groß sind -> Multiplikation > Integer.Max_Value -> Überlauf
+        }
+    }else{
+        System.out.println("Eine Zahl liegt nicht im Wertebereich");
+        // wenn eine oder beide Zahlen sind nicht in dem Wertebereich 1-2000000000
+    }
+
+
+        
+    }
+
+}
